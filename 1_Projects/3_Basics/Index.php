4c766cd9bfe592c5aa5038713e391b688c4a29af
supercgeek <cameronbb3@gmail.com>
2015-03-22 00:21:08 -0400

    New Page

--- 1_Projects/3_Basics/Index.php
+++ 1_Projects/3_Basics/Index.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
-	
+
 	<!--IDENTIFYING IFORMATION-->
-	<title>Basics | ICS4U1</title>	
+	<title>Basics | ICS4U1</title>
 	<meta name="description" content="HTML5 Page Basics">
 	<meta name="author" content="Cameron Burgess">
-	
-	<!--OUTSIDE LOAD/INSTALLS--> 
-	<link rel="stylesheet" type= "text/css" href="/Styles.css">
+
+	<!--OUTSIDE LOAD/INSTALLS-->
+	<link rel="stylesheet" type="text/css" href="/Styles.css">
+	<script src="http://use.edgefonts.net/abel:n4:all.js"></script>
 </head>
+
 <body>
 	<h1>A</h1>
 	<h2>A</h2>
 	<h3>A</h3>
-	<h4>A</h4>
-
-
+	<div id="mainText">
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis, libero eu dictum efficitur, sem lectus convallis mauris, ac faucibus risus neque vel ex. Vivamus sollicitudin luctus purus, nec accumsan augue varius eu. Suspendisse mollis eget odio in sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus eget ex suscipit, fringilla sapien sit amet, dapibus tellus. Nulla sit amet sollicitudin libero, sit amet imperdiet leo. Nulla vitae venenatis diam.
+	</p>
+	</div>
+<img src="http://41.media.tumblr.com/tumblr_mbvs79q8SJ1rzx3rlo1_1280.jpg">
+	
 	<!--JQUERY-->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </body>
+
 </html>
