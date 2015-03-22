b7706c9f3b6c8002b0694f5b01b0d718097a3bfb
supercgeek <cameronbb3@gmail.com>
2015-03-21 21:13:53 -0400

    beautify

--- Index.php
+++ Index.php
@@ -1,34 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
-	
+
 	<!--IDENTIFYING IFORMATION-->
-	<title>Cameron Burgess | ICS4U1</title>	
+	<title>Cameron Burgess | ICS4U1</title>
 	<meta name="description" content="Cameron Burgess' ICS4U1 Hub">
 	<meta name="author" content="Cameron Burgess">
-	
-	<!--OUTSIDE LOAD/INSTALLS--> 
-	<link rel="stylesheet" type= "text/css" href="styles.css">
+
+	<!--OUTSIDE LOAD/INSTALLS-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
+
 <body>
 	<!--MAIN-->
 	<h1>Cameron Burgess</h1>
 	<h3>ICS4U1 FOR MR. BRAUN</h3>
-	
+
 	<h2>Projects</h2>
 	<ul>
-		<li><a href="http://fwrd.io/1_Projects/3_Basics/Index.php">Activity 1.0 [Basics Page]</a> — <a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics"> SEE SOURCE @ GITHUB</a></li>
-		<li><a href="activity-1-1.php">Activity 1.1</a></li>
-		<li><a href="activity-1-1.php">Activity 1.2</a></li>
-		<li><a href="activity-1-1.php">Activity 1.3</a></li>
-		<li><a href="activity-1-1.php">Activity 1.4</a></li>
-		<li><a href="activity-1-1.php">Activity 1.5</a></li>
-		<li><a href="activity-1-1.php">Activity 1.7</a></li>
-		<li><a href="activity-1-1.php">Activity 1.9</a></li>
+		<li><a href="http://fwrd.io/1_Projects/3_Basics/Index.php">Activity 1.0 [Basics Page]</a>
+			<br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics">See Code Source @ GitHub</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.1</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.2</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.3</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.4</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.5</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.7</a>
+		</li>
+		<li><a href="activity-1-1.php">Activity 1.9</a>
+		</li>
 	</ul>
-	
+
 	<!--JQUERY-->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </body>
+
 </html>
