b8a1b62cba3ef7a771ade34d7b6343b5d4d35560
supercgeek <cameronbb3@gmail.com>
2015-03-21 20:43:33 -0400

    New page

---
+++ 1_Projects/3_Basics/Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<!--IDENTIFYING IFORMATION-->
+	<title>Basics | ICS4U1</title>	
+	<meta name="description" content="HTML5 Page Basics">
+	<meta name="author" content="Cameron Burgess">
+	
+	<!--OUTSIDE LOAD/INSTALLS--> 
+	<link rel="stylesheet" type= "text/css" href="/Styles.css">
+</head>
+<body>
+	<h1>A</h1>
+	<h2>A</h2>
+	<h3>A</h3>
+	<h4>A</h4>
+
+
+	<!--JQUERY-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+</html>
