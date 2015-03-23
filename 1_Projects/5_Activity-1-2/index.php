fc9eb650c1efd93c1f5aa0404065860150aae627
supercgeek <cameronbb3@gmail.com>
2015-03-23 02:41:12 -0400

    more updates.

---
+++ 1_Projects/5_Activity-1-2/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <!--IDENTIFYING IFORMATION-->
+    <title>Activity 1.2 - Cameron Burgess</title>
+    <meta name="description" content="Activity 1.2">
+    <meta name="author" content="Cameron Burgess">
+
+    <!--OUTSIDE LOAD/INSTALLS-->
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    <!--MAIN-->
+    <h1>Manipulating Images</h1>
+
+    <!--MAIN TABLE-->
+    <table style="width:5%" border="1" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+		<!--CELL ONE-->
+		<img alt="Be Careful" title="Be Careful" id="one" src="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+		<p>This is the original image.</p>
+            </td>
+            <td>
+        <!--CELL TWO-->
+		<img alt="Be Careful" title="Be Careful" id="two" src="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+		<p>This image is 100px by 100px.</p>
+            </td>
+
+        </tr>
+        <tr>
+            <td>
+               <!--CELL THREE-->
+		<img alt="Be Careful" title="Be Careful" id="three" src="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+		<p>This image is scaled to have a fixed width of 100px.</p>
+            </td>
+            <td>
+                <!--CELL FOUR-->
+		<img alt="Be Careful" title="Be Careful" id="four" src="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+		<p>This image is scaled to have a fixed height of 100px.</p>
+            </td>
+
+        </tr>
+        <tr>
+            <td>
+                <a href="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+                    <img src="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+                </a>
+            </td>
+            <td>
+                <a href="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+                    <img src="http://fc09.deviantart.net/fs70/i/2010/343/5/e/be_careful_by_antontang-d2nq7yy.jpg">
+                </a>
+            </td>
+
+        </tr>
+    </table>
+    <!--JQUERY-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+
+</html>
