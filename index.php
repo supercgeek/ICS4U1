e11d50d5e771b10becf7ddc14f5835c2bf34ddd8
supercgeek <cameronbb3@gmail.com>
2015-03-23 01:52:01 -0400

    up

--- index.php
+++ index.php
@@ -23,14 +23,14 @@
 
     <ul>
         <!--SECTION TWO PROJECTS ARE BELOW (INTRODUCTION)-->
-		<h3 id="S-HEADING">Section TWO</h3>
+        <h3 id="S-HEADING">Section TWO</h3>
         <li><a href="http://fwrd.io/1_Projects/3_Basics/index.php">Activity 1.0 "Basics Page"</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics">See Code Source ⎋</a>
         </li>
         <li><a href="http://fwrd.io/1_Projects/4_Activity-1-1/activity-1-1.php">Activity 1.1</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/4_Activity-1-1">See Code Source ⎋</a>
         </li>
-		<!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
+        <!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
         <h3 id="S-HEADING">Section THREE</h3>
         <li><a href="[LINK]">Activity 1.2</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
@@ -39,28 +39,29 @@
 		<li><a href="[LINK]">Activity 1.3</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
         </li>
-		<li><a href="[LINK]">Activity 1.4</a>
+        <li><a href="[LINK]">Activity 1.4</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
         </li>
-		<li><a href="[LINK]">Activity 1.5</a>
+        <li><a href="[LINK]">Activity 1.5</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
         </li>
-		<li><a href="[LINK]">Activity 1.6</a>
+        <li><a href="[LINK]">Activity 1.6</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
         </li>
-		<li><a href="[LINK]">Activity 1.7</a>
+        <li><a href="[LINK]">Activity 1.7</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
         </li>
-		<li><a href="[LINK]">Activity 1.7.5</a>
+        <li><a href="[LINK]">Activity 1.7.5</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
         </li>
-		<li><a href="[LINK]">Activity 1.9</a>
+        <li><a href="[LINK]">Activity 1.9</a>
             <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
-        </li>-->
-		<!--SECTION FOUR PROJECTS ARE BELOW (PHP)-->
-		<!--SECTION FIVE PROJECTS ARE BELOW (MYSQP DATABASES)-->
-		<!--SECTION SIX PROJECTS ARE BELOW (OTHER STUFF)-->
-		<!--PERFORMANCE TASK-->
+        </li>
+		-->
+        <!--SECTION FOUR PROJECTS ARE BELOW (PHP)-->
+        <!--SECTION FIVE PROJECTS ARE BELOW (MYSQP DATABASES)-->
+        <!--SECTION SIX PROJECTS ARE BELOW (OTHER STUFF)-->
+        <!--PERFORMANCE TASK-->
     </ul>
     <!--JQUERY-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
