ddae548df959443a538ff00c098db2f59ef79591
supercgeek <cameronbb3@gmail.com>
2015-03-23 01:18:15 -0400

    Index ready.

--- index.php
+++ index.php
@@ -2,40 +2,67 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
 
-	<!--IDENTIFYING IFORMATION-->
-	<title>Cameron Burgess | ICS4U1</title>
-	<meta name="description" content="Cameron Burgess' ICS4U1 Hub">
-	<meta name="author" content="Cameron Burgess">
+    <!--IDENTIFYING IFORMATION-->
+    <title>Cameron Burgess | ICS4U1</title>
+    <meta name="description" content="Cameron Burgess' ICS4U1 Hub">
+    <meta name="author" content="Cameron Burgess">
 
-	<!--OUTSIDE LOAD/INSTALLS-->
-	<link rel="stylesheet" type="text/css" href="styles.css">
+    <!--OUTSIDE LOAD/INSTALLS-->
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
-	<!--MAIN-->
-	<h1>Cameron Burgess</h1>
-	<h3>ICS4U1 FOR MR. BRAUN</h3>
+    <!--MAIN-->
+    <h1>Cameron Burgess</h1>
+    <h3>ICS4U1 FOR MR. BRAUN</h3>
 
-	<!--All project links-->
-	<h2>Projects</h2>
-	<ul>
-		<li><a href="http://fwrd.io/1_Projects/3_Basics/index.php">Activity 1.0 "Basics Page"</a>
-			<br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics">See Code Source ⎋</a>
-		</li>
-		<li><a href="http://fwrd.io/1_Projects/4_Activity-1-1/activity-1-1.php">Activity 1.1</a>
-			<br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/4_Activity-1-1">See Code Source ⎋</a>
-		</li>
-		<li><a href="[LINK]">Activity 1.2</a>
-			<br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
-		</li>
-		<li><a href="[LINK]">Activity 1.3</a>
-			<br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
-	</ul>
+    <!--All project links-->
+    <h2>Projects</h2>
 
-	<!--JQUERY-->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <ul>
+        <!--SECTION TWO PROJECTS ARE BELOW (INTRODUCTION)-->
+		<h3 id="S-HEADING">Section 2</h3>
+        <li><a href="http://fwrd.io/1_Projects/3_Basics/index.php">Activity 1.0 "Basics Page"</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics">See Code Source ⎋</a>
+        </li>
+        <li><a href="http://fwrd.io/1_Projects/4_Activity-1-1/activity-1-1.php">Activity 1.1</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/4_Activity-1-1">See Code Source ⎋</a>
+        </li>
+		<!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
+        <h3 id="S-HEADING">Section 3</h3>
+        <li><a href="[LINK]">Activity 1.2</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+        <li><a href="[LINK]">Activity 1.3</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<li><a href="[LINK]">Activity 1.4</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<li><a href="[LINK]">Activity 1.5</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<li><a href="[LINK]">Activity 1.6</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<li><a href="[LINK]">Activity 1.7</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<li><a href="[LINK]">Activity 1.7.5</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<li><a href="[LINK]">Activity 1.9</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        </li>
+		<!--SECTION FOUR PROJECTS ARE BELOW (PHP)-->
+		<!--SECTION FIVE PROJECTS ARE BELOW (MYSQP DATABASES)-->
+		<!--SECTION SIX PROJECTS ARE BELOW (OTHER STUFF)-->
+		<!--PERFORMANCE TASK-->
+    </ul>
+    <!--JQUERY-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </body>
 
 </html>
