7650dc5b63e548759c9ec58fff3fd1ff53263cf7
supercgeek <cameronbb3@gmail.com>
2015-03-23 03:12:11 -0400

    updated index

--- index.php
+++ index.php
@@ -32,8 +32,8 @@
         </li>
         <!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
         <h3 id="S-HEADING">Section THREE</h3>
-        <li><a href="[LINK]">Activity 1.2</a>
-            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+        <li><a href="http://fwrd.io/1_Projects/5_Activity-1-2/index.php">Activity 1.2</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/5_Activity-1-2">See Code Source ⎋</a>
         </li>
         <!--
 		<li><a href="[LINK]">Activity 1.3</a>
