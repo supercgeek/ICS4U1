3c9b8df38e59b8afbb30023a4627a8e7c9f3add7
supercgeek <cameronbb3@gmail.com>
2015-03-26 01:33:41 -0400

    Poems are cool.

--- 1_Projects/3_Basics/index.php
+++ 1_Projects/3_Basics/index.php
@@ -27,11 +27,19 @@
 	<!--Message-->
 	<div id="mainText">
 		<p>
-			Hi Mr. Braun,
-			<br></br>Sorry about taking so long to get started. I've had a bamboziling start to the year. Anyhow, time to begin — I can't wait to hear your feedback.
-			<br></br>Cheers,
-			<br>Cameron Burgess
-			<br></br> P.S. You gotta' love unicode.
+			Do not go gentle into that good night <br></br>
+
+Do not go gentle into that good night,<br>Old age should burn and rave at close of day;<br>Rage, rage against the dying of the light.<br></br>
+
+Though wise men at their end know dark is right,<br>Because their words had forked no lightning they<br>Do not go gentle into that good night.<br></br>
+
+Good men, the last wave by, crying how bright<br>Their frail deeds might have danced in a green bay,<br>Rage, rage against the dying of the light.<br></br>
+
+Wild men who caught and sang the sun in flight,<br>And learn, too late, they grieved it on its way,<br>Do not go gentle into that good night.<br></br>
+
+Grave men, near death, who see with blinding sight<br>Blind eyes could blaze like meteors and be gay,<br>Rage, rage against the dying of the light.<br></br>
+
+And you, my father, there on the sad height,<br>Curse, bless, me now with your fierce tears, I pray.<br>Do not go gentle into that good night.<br>Rage, rage against the dying of the light.
 		</p>
 	</div>
 	<!--A clickable picture-->
