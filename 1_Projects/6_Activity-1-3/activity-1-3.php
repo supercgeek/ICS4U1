05be38de4e95522a416ffd10d714022072c13c66
supercgeek <cameronbb3@gmail.com>
2015-03-29 03:00:32 -0400

    More updates

--- 1_Projects/6_Activity-1-3/activity-1-3.php
+++ 1_Projects/6_Activity-1-3/activity-1-3.php
@@ -12,52 +12,49 @@
     <!--OUTSIDE LOAD/INSTALLS-->
 </head>
 
-<body style="background-color:#34495e; color: #ffffff; padding: 30px; font-family: Calibri,Candara,Segoe,"Segoe UI",Optima,Arial,sans-serif">
+<body style="background-color:#34495e; color: #ffffff; padding: 30px; font-family: Calibri, Candara, Segoe, Segoe UI, Optima,Arial,sans-serif">
     <!--MAIN-->
     <h1>Styling & Formatting HTML Pages</h1>
-	<hr><br>
+    <hr>
+    <br>
 
-<!-- First Block-->
-<P style="font-family: Avant Garde, sans-serif; padding: 10px; color: #8e44ad; background-color: #ffffff">
-In this paragraph the font has been set to Avant Garde, sans-serif; and has a padding of 10px and the font color is #8e44ad and the background color is #ffffff
-</p>
+    <!-- First Block-->
+    <P style="font-family: Avant Garde, sans-serif; padding: 10px; color: #8e44ad; background-color: #ffffff">
+        In this paragraph the font has been set to Avant Garde, sans-serif; and has a padding of 10px and the font color is #8e44ad and the background color is #ffffff
+    </p>
 
-<!-- Second Block-->
-<h2 style="padding: 30px; color: #f39c12; background-color: #2980b9">
-This is a size 2 heading with a #2980b9 color background and
-padding of 30px and a font color of #f39c12</h2>	
+    <!-- Second Block-->
+    <h2 style="padding: 30px; color: #f39c12; background-color: #2980b9">
+This is a size 2 heading with a #2980b9 color background and padding of 30px and a font color of #f39c12</h2>	
 
-<!--Third Block-->
-<h3 style="padding: 5px; color: #ffffff; background-color: #888888; width: 700px; font-family: "Comic Sands MS", cursive, sans-serif;">
-This is a size 2 heading with a #2980b9 color background and
-padding of 30px and a font color of #f39c12</h3>	
+    <!--Third Block-->
+    <h3 style="padding: 5px; color: #ffffff; background-color: #888888; width: 700px; font-family: Comic Sans MS, cursive, sans-serif;">
+This is a size 2 heading with a #2980b9 color background and padding of 30px and a font color of #f39c12</h3>	
 
-<!--Fourth Block-->
-<P style="font-family: Courier; color: #d35400; background-color: #ffffff; font-size: 2.0em;">
-In this paragraph the font has been set to a size of 2.0em, color of #d35400 and courier font style.
-</p>
+    <!--Fourth Block-->
+    <P style="font-family: Courier; color: #d35400; background-color: #ffffff; font-size: 2.0em;">
+        In this paragraph the font has been set to a size of 2.0em, color of #d35400 and courier font style.
+    </p>
 
-<!--Fifth Block-->
-<P style="color: #d35400; background-color: #1abc9c; color: #ffffff; padding: 5px;">
+    <!--Fifth Block-->
+    <P style="color: #d35400; background-color: #1abc9c; color: #ffffff; padding: 5px;">
+        This paragraph has a background color of #1abc9c and font color #ffffff and the paragraph is padded by 5px. Let's step this up a notch and look at styling tables. Below we are going to create a 3 column, 1 row table with a border size of 2, cellpadding of 30 and cellspacing of 0
+    </p>
+    <table border="2" cellpadding="30" cellspacing="0">
+        <tr>
+            <td style="background-color: #ffffff; color: #000000">
+                #ffffff background and #000000 font color
+            </td>
+            <td style="background-color: #000000; color: #ffffff">
+                #000000 background and #ffffff font color
+            </td>
+            <td style="background-color: #f39c12; color: #333333">
+                #f39c12 background and #333333 font color
+            </td>
+        </tr>
 
-This paragraph has a background color of #1abc9c and font color #ffffff and the paragraph is padded by 5px. Let's step this up a notch and look at styling tables. Below we are going to create a 3 column, 1 row table with a border size of 2, cellpadding of 30 and cellspacing of 0
+    </table>
 
-</p>
-	<table border="2" cellpadding="30" cellspacing="0">
-	<tr>
-		<td style="background-color: #ffffff; color: #000000">
-		#ffffff background and #000000 font color
-		</td>
-		<td style="background-color: #000000; color: #ffffff">
-		#000000 background and #ffffff font color
-		</td>
-		<td style="background-color: #f39c12; color: #333333">
-		#f39c12 background and #333333 font color
-		</td>
-		</tr>	
-
-	</table>
-	
     <!--JQUERY-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </body>
