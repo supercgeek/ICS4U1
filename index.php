9bfbb5563dc188940a8f1d59f0c8179b80f8527d
supercgeek <cameronbb3@gmail.com>
2015-03-29 19:33:10 -0400

    not needed

--- index.php
+++ index.php
@@ -24,33 +24,33 @@
     <ul>
         <!--SECTION TWO PROJECTS ARE BELOW (INTRODUCTION)-->
         <h3 id="S-HEADING">Section TWO</h3>
-        <li><a href="http://fwrd.io/1_Projects/3_Basics/index.php">Activity 1.0 "Basics Page"</a>
+        <li><a href="/1_Projects/3_Basics/index.php">Activity 1.0 "Basics Page"</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics">See Code Source ⎋</a>
         </li>
-        <li><a href="http://fwrd.io/1_Projects/4_Activity-1-1/activity-1-1.php">Activity 1.1</a>
+        <li><a href="/1_Projects/4_Activity-1-1/activity-1-1.php">Activity 1.1</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/4_Activity-1-1">See Code Source ⎋</a>
         </li>
         <!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
         <h3 id="S-HEADING">Section THREE</h3>
-        <li><a href="http://fwrd.io/1_Projects/5_Activity-1-2/activity-1-2.php">Activity 1.2</a>
+        <li><a href="/1_Projects/5_Activity-1-2/activity-1-2.php">Activity 1.2</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/5_Activity-1-2">See Code Source ⎋</a>
         </li>
         
-		<li><a href="http://fwrd.io/1_Projects/6_Activity-1-3/activity-1-3.php">Activity 1.3</a>
+		<li><a href="/1_Projects/6_Activity-1-3/activity-1-3.php">Activity 1.3</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/6_Activity-1-3">See Code Source ⎋</a>
         </li>
         
 		
-        <li><a href="http://fwrd.io/1_Projects/7_Activity-1-4/activity-1-4.php">Activity 1.4</a>
+        <li><a href="/1_Projects/7_Activity-1-4/activity-1-4.php">Activity 1.4</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/7_Activity-1-4">See Code Source ⎋</a>
 		</li>
         
 		
-		<li><a href="http://fwrd.io/1_Projects/8_Activity-1-5/activity-1-5.php">Activity 1.5</a>
+		<li><a href="/1_Projects/8_Activity-1-5/activity-1-5.php">Activity 1.5</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/8_Activity-1-5">See Code Source ⎋</a>
         </li>
         
-        <li><a href="http://fwrd.io/1_Projects/9_Activity-1-7/activity-1-7.php">Activity 1.7</a>
+        <li><a href="/1_Projects/9_Activity-1-7/activity-1-7.php">Activity 1.7</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/9_Activity-1-7">See Code Source ⎋</a>
         </li>
 		<!--
