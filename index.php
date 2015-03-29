9e9ac1d82d1cb604554da44fb71562b3ddee3fd0
supercgeek <cameronbb3@gmail.com>
2015-03-29 19:41:34 -0400

    index fixes

--- index.php
+++ index.php
@@ -25,37 +25,37 @@
         <!--SECTION TWO PROJECTS ARE BELOW (INTRODUCTION)-->
         <h3 id="S-HEADING">Section TWO</h3>
         <li><a href="/1_Projects/3_Basics/index.php">Activity 1.0 "Basics Page"</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/3_Basics">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/03_Basics">See Code Source ⎋</a>
         </li>
         <li><a href="/1_Projects/4_Activity-1-1/activity-1-1.php">Activity 1.1</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/4_Activity-1-1">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/04_Activity-1-1">See Code Source ⎋</a>
         </li>
         <!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
         <h3 id="S-HEADING">Section THREE</h3>
         <li><a href="/1_Projects/5_Activity-1-2/activity-1-2.php">Activity 1.2</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/5_Activity-1-2">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/05_Activity-1-2">See Code Source ⎋</a>
         </li>
         
 		<li><a href="/1_Projects/6_Activity-1-3/activity-1-3.php">Activity 1.3</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/6_Activity-1-3">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/06_Activity-1-3">See Code Source ⎋</a>
         </li>
         
 		
         <li><a href="/1_Projects/7_Activity-1-4/activity-1-4.php">Activity 1.4</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/7_Activity-1-4">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/07_Activity-1-4">See Code Source ⎋</a>
 		</li>
         
 		
 		<li><a href="/1_Projects/8_Activity-1-5/activity-1-5.php">Activity 1.5</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/8_Activity-1-5">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/08_Activity-1-5">See Code Source ⎋</a>
         </li>
         
         <li><a href="/1_Projects/9_Activity-1-7/activity-1-7.php">Activity 1.7</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/9_Activity-1-7">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/09_Activity-1-7">See Code Source ⎋</a>
         </li>
 		
         <li><a href="[LINK]">Activity 1.8 "Divs and IDs Practice"</a>
-            <br><a target="_blank" class="source" href="[LINK]">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/10_Activity-1-8">See Code Source ⎋</a>
         </li>
 		<!--
         <li><a href="[LINK]">Activity 1.9</a>
