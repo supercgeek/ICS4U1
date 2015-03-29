c36f4bfaac8b530b42b94aa9bd6ac1ec16be3e00
Cameron Burgess <cameronbb3@gmail.com>
2015-03-29 01:18:50 -0400

    Document folder updates

---
+++ 1_Projects/6_Activity-1-3/activity-1-3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <!--IDENTIFYING IFORMATION-->
+    <title>Activity 1.3 - Cameron Burgess</title>
+    <meta name="description" content="Activity 1.3">
+    <meta name="author" content="Cameron Burgess">
+
+    <!--OUTSIDE LOAD/INSTALLS-->
+</head>
+
+<body style="background-color:#34495e; text-color: #ffffff; padding: 30px; font-family: Calibri">
+    <!--MAIN-->
+    <h1>Styling & Formatting HTML Pages</h1>
+    <!--JQUERY-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+
+</html>
