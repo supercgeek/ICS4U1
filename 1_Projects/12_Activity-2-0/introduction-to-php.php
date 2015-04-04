1760b2bec1705c46cccbe9e440432a01433511a5
Cameron Burgess <cameronbb3@gmail.com>
2015-04-04 03:35:28 -0400

    2.0 Done

---
+++ 1_Projects/12_Activity-2-0/introduction-to-php.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+   
+<html lang="en">
+<head>
+	<title>Introduction to PHP</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	
+    <!--STYLESHEET LOAD-->
+	<link rel="stylesheet" type="text/css" href="styles_intro.css">
+    <script src="http://use.edgefonts.net/viga:n4:all.js"></script>
+    
+</head>
+<body>
+<?
+	echo "<h2>What is PHP?</h2>
+	<p><b>PHP</b> (recursive acronym for <b>P</b>HP: <b>H</b>ypertext <b>P</b>reprocessor) is a widely-used open source general-purpose scripting language that is especially suited for web development and can be embedded into HTML.</p>";
+?>
+
+<h2>Simple Form Validation with PHP</h2>
+
+<?
+	/*
+    the if statement below makes the code inside of it
+	only show up if the button in the form named 'submitBtn' is clicked.
+	You can have multiple submit buttons, but they must have different names.
+    */
+	if ($_POST['submitBtn1']) {
+		// set the variables...
+		$theName = $_POST['theName'];
+		$socksColour = $_POST['socksColour'];
+        $emailAdd = $_POST['email'];
+		$favSoda = $_POST['favSoda'];
+        $tired = $_POST['tired'];
+        
+		// print out the message
+		echo "<p class='output'>The name that was entered into the form was: <b style='background: #f1c40f;'>" . $theName . "</b><br />
+		The colour socks chosen was: <b style='background: #f1c40f;'>" . $socksColour . "<br></b>
+        The email address inputted was: <b style='background: #f1c40f;'>" . $emailAdd . "</b> <br>
+        Your favourite soda is: <b style='background: #f1c40f;'>" . $favSoda . "</b> <br>
+        You are <b style='background: #f1c40f;'>" . $tired . "</b> right now.</p>";
+	}
+?>
+
+ <h2>The Form</h2>
+ <p>Complete the form below and press submit.  Notice how the values are now output (echoed) to the page based and the variable name assigned in the form.</p>
+<h3>Form Attributes</h3>
+ <ul>
+	<li> "name" is the unique name of the form</li>
+	<li> "get" method shows the variables and values in the address bar of your browser</li>
+	<li> "post" method hides the type variables and values - the information is posted to the server</li>
+	<li> "action" is the URL that the form will direct to once the submit button is pressed</li>
+</ul>
+
+<form name="form1" action="introduction-to-php.php" method="post">
+	<b>Name:</b><br />
+	<input type="text" name="theName" value="" class="fancyInput"></input><br />
+	<b>Colour of Socks:</b><br />
+	<select name="socksColour" class="fancyInput">
+		<option value=""></option>
+		<option value="blue">Blue Socks</option>
+		<option value="green">Green Socks</option>
+		<option value="red">Red Socks</option>
+	</select><br />
+    <b>Favourite type of Soda:</b><br />
+	<select name="favSoda" class="fancyInput">
+		<option value=""></option>
+		<option value="drP">Dr. Pepper</option>
+		<option value="pepsi">Pepsi</option>
+		<option value="yumm">Yumm Rocks</option>
+        <option value="gt">Green Tea</option>
+	</select><br />
+    <b>Email:</b><br>
+    <input type="text" name="email" value="" class="fancyInput"></input></br>
+    <b>Tired?</b><br>
+    <input type="radio" name="tired" value="tired" class="fancyInput" required>Yep</input><br><br>
+    <input type="radio" name="tired" value="not tired" class="fancyInput" required>Nope</input><br><br>
+    
+
+    <!--SUBMIT BUTTON-->
+	<input type="submit" value="SUBMIT" name="submitBtn1" class="subButton"></input>
+</form>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -24,47 +24,69 @@
     <ul>
         <!--SECTION TWO PROJECTS ARE BELOW (INTRODUCTION)-->
         <h3 id="S-HEADING">Section TWO</h3>
+
         <li><a href="/1_Projects/03_Basics/index.php">Activity 1.0 "Basics Page"</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/03_Basics">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/03_Basics">See Code Source</a>
         </li>
+        
         <li><a href="/1_Projects/04_Activity-1-1/activity-1-1.php">Activity 1.1</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/04_Activity-1-1">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/04_Activity-1-1">See Code Source</a>
         </li>
+        
         <!--SECTION THREE PROJECTS ARE BELOW (HTML5 & CSS3)-->
         <h3 id="S-HEADING">Section THREE</h3>
+        
         <li><a href="/1_Projects/05_Activity-1-2/activity-1-2.php">Activity 1.2</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/05_Activity-1-2">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/05_Activity-1-2">See Code Source</a>
         </li>
         
 		<li><a href="/1_Projects/06_Activity-1-3/activity-1-3.php">Activity 1.3</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/06_Activity-1-3">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/06_Activity-1-3">See Code Source</a>
         </li>
         
 		
         <li><a href="/1_Projects/07_Activity-1-4/activity-1-4.php">Activity 1.4</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/07_Activity-1-4">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/07_Activity-1-4">See Code Source</a>
 		</li>
         
 		
 		<li><a href="/1_Projects/08_Activity-1-5/activity-1-5.php">Activity 1.5</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/08_Activity-1-5">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/08_Activity-1-5">See Code Source</a>
         </li>
         
         <li><a href="/1_Projects/09_Activity-1-7/activity-1-7.php">Activity 1.7</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/09_Activity-1-7">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/09_Activity-1-7">See Code Source</a>
         </li>
 		
         <li><a href="/1_Projects/10_Activity-1-8/division-tags-and-css-practice.php">Activity 1.8 "Divs and IDs Practice"</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/10_Activity-1-8">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/10_Activity-1-8">See Code Source</a>
         </li>
 		
         <li><a href="/1_Projects/11_Activity-1-9/activity-1-9a.php">Activity 1.9 "Part A"</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/11_Activity-1-9">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/11_Activity-1-9">See Code Source</a>
         </li>
  <li><a href="/1_Projects/11_Activity-1-9/activity-1-9b.php">Activity 1.9 "Part B"</a>
-            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/11_Activity-1-9">See Code Source ⎋</a>
+            <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/11_Activity-1-9">See Code Source</a>
         </li>
         <!--SECTION FOUR PROJECTS ARE BELOW (PHP)-->
+        <h3 id="S-HEADING">Section FOUR</h3>
+        
+        <li><a href="[LINK]">Activity 2.0 "Introduction to PHP"</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source</a>
+        </li>
+        <!--
+        <li><a href="[LINK]">Activity 2.1</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source</a>
+        </li>
+        
+        <li><a href="[LINK]">Activity 2.2 "Part A"</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source</a>
+        </li>
+        
+        <li><a href="[LINK]">Activity 2.2 "Part B"</a>
+            <br><a target="_blank" class="source" href="[LINK]">See Code Source</a>
+        </li>
+        -->
         <!--SECTION FIVE PROJECTS ARE BELOW (MYSQP DATABASES)-->
         <!--SECTION SIX PROJECTS ARE BELOW (OTHER STUFF)-->
         <!--PERFORMANCE TASK-->
