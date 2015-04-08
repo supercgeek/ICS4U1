7a8ff231c3bb59dd6c4b96dfacf880adcecf9de2
supercgeek <cameronbb3@gmail.com>
2015-04-08 02:24:15 -0400

    w

--- index.php
+++ index.php
@@ -117,7 +117,6 @@
 		<li><a href="/1_Projects/18_Activity_2-4-3/activity-dice-game.php">Activity 2.4.2 "Part D"</a>
             <br><a target="_blank" class="source" href="https://github.com/supercgeek/ICS4U1/tree/master/1_Projects/18_Activity_2-4-2">See Code Source</a>
         </li>
-		
 
   		<!--SECTION FIVE PROJECTS ARE BELOW (MYSQP DATABASES)-->
         <!--
