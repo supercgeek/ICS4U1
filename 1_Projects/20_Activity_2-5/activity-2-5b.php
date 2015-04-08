b6ba57d04c346a73389bb98d3811bd1fec4cce88
supercgeek <cameronbb3@gmail.com>
2015-04-08 02:56:38 -0400

    For the love of code

---
+++ 1_Projects/20_Activity_2-5/activity-2-5b.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Activity 2.5b Output - FOR Loops</title>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+		<!--STYLESHEET LOAD-->
+		<link rel="stylesheet" type="text/css" href="styles_a.css">
+		<script src="http://use.edgefonts.net/lekton:n7,i4,n4:all.js"></script>
+	</head>
+
+	<body>
+		<h1>Activity 2.5b Output - FOR Loops</h1>
+		<p><a href="activity-2-5b.php?go=yes">Click Here to Run</a></p>
+		
+		<?php
+		// rand() function randomly picks a number between 1 and 6 and assigns it to the variable $roll
+		if ($_GET['go'] == "yes") {
+				for ($count = 0; $count < 20; $count = $count + 2){
+					if ($count == 10) {
+						echo "$count - Half way there!<br>";						
+					}
+					else {
+						echo "$count<br>";
+					}
+					
+				}
+			}
+		?>
+	</body>
+</html>
+
