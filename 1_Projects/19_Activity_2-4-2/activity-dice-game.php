22ce20cdc01f0f9eb21ba72fa4477f0dcbe97f15
supercgeek <cameronbb3@gmail.com>
2015-04-08 01:43:25 -0400

    Getting dicey

---
+++ 1_Projects/19_Activity_2-4-2/activity-dice-game.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Dice Game</title>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+
+		<!--STYLESHEET LOAD-->
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+
+	<body>
+		<h1>Dice Game!</h1>
+		<h3>Demonstrates rolling a die</h3>
+		<?php
+		// rand() function randomly picks a number between 1 and 6 and assigns it to the variable $roll
+		$roll = rand(1,6); 
+		echo "<p>You rolled a " . $roll . ". </p>";
+		echo "<img src=\"images/die" . $roll . ".gif\" alt=\"die image\">";
+		?>
+		<p>Refresh this page in the browser to roll another die.</p>
+	</body>
+</html>
