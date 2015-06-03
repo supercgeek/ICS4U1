c65215104d34306c436c132c0baa9a95de717681
Cameron Burgess <cameronbb3@gmail.com>
2015-06-03 14:25:49 -0400

    Go

--- 1_Projects/21_Activity_2-6/activity-2-6a.php
+++ 1_Projects/21_Activity_2-6/activity-2-6a.php
@@ -1,1 +1,13 @@
-activity-2-6.php
+<body>
+
+	<?php
+	function namePrinter($name, $age, $job) {
+	     echo $name . " is " . $age . " years old and he is a " . $job . ".<BR>";
+	}
+
+	namePrinter("Bob", "6", "Pre-Schooler");
+	namePrinter("Kevin", "18", "High-Schooler");
+	namePrinter("Chris", "60", "Garbage Man");
+	?>
+
+</body>
