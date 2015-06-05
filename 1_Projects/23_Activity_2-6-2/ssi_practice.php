8bd60d1ad9866534ed02850fef964a4738e69cd0
supercgeek <cameronbb3@gmail.com>
2015-06-05 03:31:46 -0400

    SSI begins

---
+++ 1_Projects/23_Activity_2-6-2/ssi_practice.php
@@ -0,0 +1,8 @@
+<body>
+<h2>Working with Server Side Includes</h2>
+<a href="ssi_practice.php?id=1">Link 1</a> | <a href="ssi_practice.php?id=2">Link 1</a>
+<?php
+// include statements go next based on link selection
+// use the id value to determine which page to show
+?>
+</body>
