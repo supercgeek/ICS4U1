613837c9162b6df2b1782c2fc21dec1bdd40e02e
supercgeek <cameronbb3@gmail.com>
2015-06-09 03:23:09 -0400

    Abstracted Extensible Array

--- 1_Projects/24_Activity_2-7/activity-2-7c.php
+++ 1_Projects/24_Activity_2-7/activity-2-7c.php
@@ -9,49 +9,69 @@
 		<script src="http://use.edgefonts.net/lekton:n7,i4,n4:all.js"></script>
 	</head>
 	<body>		
-		<h2>Planets!</h2>
-		<a href="activity-2-7b.php?planet=Mercury">Mercury</a>
-		<a href="activity-2-7b.php?planet=Venus">Venus</a>
-		<a href="activity-2-7b.php?planet=Earth">Earth</a>
-		<a href="activity-2-7b.php?planet=Mars">Mars</a>
-		<a href="activity-2-7b.php?planet=Jupiter">Jupiter</a>
-		<a href="activity-2-7b.php?planet=Saturn">Saturn</a>
-		<a href="activity-2-7b.php?planet=Uranus">Uranus</a>
-		<a href="activity-2-7b.php?planet=Neptune">Neptune</a>
-		
-		<BR><BR>
-	</form>
-
 <?php
-		//Accelerations
-		$acceleration[Mercury] = 3.76;
-		$acceleration[Venus] = 9.04;
-		$acceleration[Earth] = 9.8;
-		$acceleration[Mars] = 3.77;
-		$acceleration[Jupiter] = 23.6;
-		$acceleration[Saturn] = 10.06;
-		$acceleration[Uranus] = 8.87;
-		$acceleration[Neptune] = 11.23;
-		
-		//Planet Images
-		$planet_image[Mercury] = "http://www.independent.co.uk/incoming/article8497114.ece/binary/original/Mercury.jpg";
-		$planet_image[Venus] = "http://www.mnh.si.edu/exhibits/evolving-universe/full/Venus.jpg";
-		$planet_image[Earth] = "http://solarviews.com/raw/earth/bluemarblewest.jpg";
-		$planet_image[Mars] = "http://www.worldlawdirect.com/forum/attachments/international-law-issues/207d1288285424-who-owns-planets-mars-venus-venus.jpg";
-		$planet_image[Jupiter] = "http://burro.astr.cwru.edu/stu/media/jupiter.jpg";
-		$planet_image[Saturn] = "http://sphereboy.com/wp-content/uploads/2015/01/saturnmay2013.png";
-		$planet_image[Uranus] = "http://www.wwu.edu/skywise/planets/neptune.jpg";
-		$planet_image[Neptune] = "http://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg";
-	
-		if ($_GET['planet'] != null ) {
-			echo "<img src='" . $planet_image[($_GET['planet'])] . "'<BR>"; 		
-			echo "<BR>The acceleration due to gravity on " . ($_GET['planet']) . " is " .  $acceleration[($_GET['planet'])] . " m/s^2.<BR>";
+//DEPARTMENTS 
+// ONE: SCIENCE
+// TWO: MATH
+// THREE: ENGLISH
+// FOUR: DESIGN
+// FIVE: LAW
+// SIX: ART
+
+//DEPARTMENT NAMES
+$departmentName[0] = "Science";
+$departmentName[1] = "Math";
+$departmentName[2] = "English";
+$departmentName[3] = "Design";
+$departmentName[4] = "Law";
+$departmentName[5] = "Art";
+
+//SCIENCE DEPARTMENT
+$departments['0'][0] = "Braun";
+$departments['0'][1] = "LeBlanc";
+$departments['0'][2] = "Warner";
+$departments['0'][3] = "Anceriz";
+$departments['0'][4] = "Jones";
+$departments['0'][5] = "Kennedy";
+
+//MATH DEPARTMENT
+$departments['1'][0] = "Smith";
+$departments['1'][1] = "Platnar";
+$departments['1'][2] = "Braun";
+
+//ENGLISH
+$departments['2'][0] = "Ricardio";
+$departments['2'][1] = "Sulivan";
+
+//DESIGN
+$departments['3'][0] = "Eames";
+$departments['3'][1] = "Kim";
+$departments['3'][2] = "Qunpineac";
+
+//LAW
+$departments['4'][0] = "Polver";
+$departments['4'][1] = "Nero";
+$departments['4'][2] = "Polver";
+$departments['4'][3] = "Nero";
+
+//ART
+$departments['5'][0] = "Ali";
+$departments['5'][1] = "Boneto";
+
+
+// display the information in the array
+echo "<h2>Working with Multi-dimensional Arrays</h2>";
+echo "The array \"departments\" has " . count($departments) . " departments.";
+echo "<br />In total there are " . count($departments, COUNT_RECURSIVE) . " teachers.";
+
+for ($e = 0; $e < (count($departments)); $e++) {
+	echo "<p>The " . $departmentName[$e] . " deparment consists of the following teachers:</p>";
+	echo "<ol>";
+		for ($c = 0; $c < (count($departments[$e])); $c++) {
+		echo "<li /> " . $departments[$e][$c];
 		}
-
-//for ($i = 0; $i < sizeof($course); $i++) {
-//     echo "<li>" . $course[$i] . "</li>";
-//}
+	echo "</ol>";
+}		
 ?>
-
 	</body>
 </html>
