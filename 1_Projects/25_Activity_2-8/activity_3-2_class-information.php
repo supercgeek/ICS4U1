1bf7e8dd9514e8114d17ab1fc976c69969aaac27
supercgeek <cameronbb3@gmail.com>
2015-06-13 22:16:07 -0400

    age thing

--- 1_Projects/25_Activity_2-8/activity_3-2_class-information.php
+++ 1_Projects/25_Activity_2-8/activity_3-2_class-information.php
@@ -17,10 +17,19 @@ $sql = "SELECT * FROM class_information"; // * == all
 $result = mysql_query($sql, $db);
 
 echo "<table>"; // start the table outside of the while loop
-echo "<tr><td>Student ID</td><td>First Name</td><td>Last Name</td><td>Age</td><td>Favourite Food</td><td>Last Updated</td></tr>";
+echo "<tr><td>Student ID</td><td>First Name</td><td>Last Name</td><td>Age</td><td>Favourite Food</td><td>Comment</td><td>Last Updated</td></tr>";
 // basic output of the data from the table
-while ($myrow = mysql_fetch_array($result)) { 
-	echo "<tr><td>" . $myrow['student_id'] . "</td><td>" . $myrow['first_name'] . "</td><td>" . $myrow['last_name'] . "</td><td>" . $myrow['age'] . "</td><td>" . $myrow['fav_food'] . "</td><td>" . $myrow['updated'] . "</td></tr>";
+while ($myrow = mysql_fetch_array($result)) {
+	if ($myrow['age'] < 6) {$comment = "You're basically a toddler.";}
+	else if ($myrow['age'] > 5 && $myrow['age'] < 11) {$comment = "A young child.";}
+	else if ($myrow['age'] >= 11 && $myrow['age'] < 16) {$comment = "Tween going teen.";}
+	else if ($myrow['age'] >= 16 && $myrow['age'] < 20) {$comment = "Eh?-dult.";}
+	else if ($myrow['age'] >= 21 && $myrow['age'] < 25) {$comment = "Adult.";}
+	else if ($myrow['age'] >= 26 && $myrow['age'] < 30) {$comment = "Adult.";}
+	else {$comment = "Prime Dult";}
+	
+	
+	echo "<tr><td>" . $myrow['student_id'] . "</td><td>" . $myrow['first_name'] . "</td><td>" . $myrow['last_name'] . "</td><td>" . $myrow['age'] . "</td><td>" . $myrow['fav_food'] . "</td><td>" . $comment . "</td><td>" . $myrow['updated'] . "</td></tr>";
 }
 echo "</table>"; // end the table outside of the loop
 
