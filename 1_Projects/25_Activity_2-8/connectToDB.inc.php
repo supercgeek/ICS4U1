d1cd892c5fc4d0ef7c2dec9d9672f5c300d6fc75
supercgeek <cameronbb3@gmail.com>
2015-06-13 21:27:16 -0400

    Flags are wavin

--- 1_Projects/25_Activity_2-8/connectToDB.inc.php
+++ 1_Projects/25_Activity_2-8/connectToDB.inc.php
@@ -1,9 +1,15 @@
 <?php
-// database configurations
+// database configurations (LIVE ON-LINE)
 $db_servername = "external-db.s205379.gridserver.com";
 $db_username = "db205379";
 $db_password = "5Kz/0o3Pn1s'qusk~r3.";
 $db_databasename = "db205379_supercgeek";
+
+// database configurations (LOCAL HOST DEV) [DEFUNCT]
+//$db_servername = "internal-db.s205379.gridserver.com";
+//$db_username = "db205379";
+//$db_password = "5Kz/0o3Pn1s'qusk~r3.";
+//$db_databasename = "db205379_supercgeek";
 
 //************************************************************************
 // Connect to MySQL Database
@@ -14,6 +20,6 @@ if (!$db) {
     die('Could not connect to database: ' . mysql_error());
 } else {
     mysql_select_db($db_databasename, $db);
-    echo "<B>Successfully connected to the database!</B><BR><BR>"; // remove this in real scripts
+//    echo "<B>Successfully connected to the database!</B><BR><BR>"; // remove this in real scripts
 }
 ?>
