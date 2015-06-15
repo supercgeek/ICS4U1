df1b828bfdc25829ed65ea6abf4849c3aa9facec
supercgeek <cameronbb3@gmail.com>
2015-06-15 11:05:10 -0400

    Ello

--- 1_Projects/27_Activity_3/perf-task-2-fastfood.php
+++ 1_Projects/27_Activity_3/perf-task-2-fastfood.php
@@ -10,23 +10,42 @@
 	</head>
 <body>
 <h1>Fast Food Yums!</h1><h3>Online Order Form</h3><BR><BR>
+
+<!--	
 <div class = "box">
 	<img src="http://pixel.brit.co/wp-content/uploads/2013/05/8-BlackBurger.jpg">
 	<h2>Cheese Burger</h2>
 	<!--<BR>-->
-	<p class = "price"><strong>$8.99</strong></p>
-	ORDER: <input type = "number" min = "0">
+	<!--<p class = "price"><strong>$8.99</strong></p>
+	ORDER: <input type = "number" min = "0" name = "orderVal1" value = "" ></input>
 </div>
 
 <div class = "box">
-	<img src="http://pixel.brit.co/wp-content/uploads/2013/05/8-BlackBurger.jpg">
-	<h2>Cheese Burger</h2>
+	<img src="http://www.fitbie.com/sites/default/files/worst-pasta-dishes-stouffers-fettucine-alfredo-ss.jpg">
+	<h2>Pasta</h2>
 	<!--<BR>-->
-	<p class = "price"><strong>$8.99</strong></p>
-</div>
+	<!--<p class = "price"><strong>$6.99</strong></p>
+		ORDER: <input type = "number" min = "0" name = "orderVal2" value = "" ></input>
+</div>-->
+
 
 <?php
 include("DBC.php");
 
+$sql = "SELECT * FROM restaurant_menu_items";
+$result = mysql_query($sql, $db);
+echo "<form action = 'perf-task-2-fastfood.php' method = 'get'>";
+while ($row = mysql_fetch_array($result)) {
+	echo "<div class = 'box'>
+	<img src = '" . $row['img'] .  "'>
+	<h2>" . $row['item'] .  "</h2>
+	<p class = 'price'><strong>$" . $row['cost'] .  "</strong></p>
+	ORDER: <input type = 'number' min = '0' name = '" . $row['item'] .  "Order_" . $row['item_id'] .  "' value = '' ></input>
+	</div>";
+	
+}
+echo "</form>";
+
+echo "";
 ?>
 </body>
