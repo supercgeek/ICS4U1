02dd5015aa6e045710b8c13b5fcbecee54eeb8db
supercgeek <cameronbb3@gmail.com>
2015-06-15 10:15:06 -0400

    changes

--- 1_Projects/27_Activity_3/perf-task-2-fastfood.php
+++ 1_Projects/27_Activity_3/perf-task-2-fastfood.php
@@ -9,8 +9,21 @@
 		<script src="http://use.edgefonts.net/lekton:n7,i4,n4:all.js"></script>
 	</head>
 <body>
-<h1>Fast Food Yums!</h1>
+<h1>Fast Food Yums!</h1><h3>Online Order Form</h3><BR><BR>
+<div class = "box">
+	<img src="http://pixel.brit.co/wp-content/uploads/2013/05/8-BlackBurger.jpg">
+	<h2>Cheese Burger</h2>
+	<!--<BR>-->
+	<p class = "price"><strong>$8.99</strong></p>
+	ORDER: <input type = "number" min = "0">
+</div>
 
+<div class = "box">
+	<img src="http://pixel.brit.co/wp-content/uploads/2013/05/8-BlackBurger.jpg">
+	<h2>Cheese Burger</h2>
+	<!--<BR>-->
+	<p class = "price"><strong>$8.99</strong></p>
+</div>
 
 <?php
 include("DBC.php");
