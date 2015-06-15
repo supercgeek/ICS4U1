c55875bfc5dbc32f4bbb55d86b9e7c747217e094
supercgeek <cameronbb3@gmail.com>
2015-06-15 16:52:56 -0400

    close

--- 1_Projects/27_Activity_3/perf-task-1-scholarships.php
+++ 1_Projects/27_Activity_3/perf-task-1-scholarships.php
@@ -95,42 +95,42 @@
 	<!--MARK 1-->
 	<div class = "box">
 		<h2>Couse 1</h2><p class = "mono">
-		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark1" value = "" ></input></strong></p>
+		MARK: <input type = "number" step = "1" placeholder = "##%" required min = "0" max = "100" name = "mark1" value = "" ></input></strong></p>
 	</div>
 	
 	<!--MARK 2-->
 	<div class = "box">
 		<h2>Couse 2</h2><p class = "mono">
-		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark2" value = "" ></input></strong></p>
+		MARK: <input type = "number" step = "1" placeholder = "##%" required min = "0" max = "100" name = "mark2" value = "" ></input></strong></p>
 	</div>
 	
 	<!--MARK 3-->
 	<div class = "box">
 		<h2>Couse 3</h2><p class = "mono">
-		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark3" value = "" ></input></strong></p>
+		MARK: <input type = "number" step = "1" placeholder = "##%" required min = "0" max = "100" name = "mark3" value = "" ></input></strong></p>
 	</div>
 	
 	<!--MARK 4-->
 	<div class = "box">
 		<h2>Couse 4</h2><p class = "mono">
-		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark4" value = "" ></input></strong></p>
+		MARK: <input type = "number" step = "1" placeholder = "##%" required min = "0" max = "100" name = "mark4" value = "" ></input></strong></p>
 	</div>
 	
 	<!--MARK 5-->
 	<div class = "box">
 		<h2>Couse 5</h2><p class = "mono">
-		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark5" value = "" ></input></strong></p>
+		MARK: <input type = "number" step = "1" placeholder = "##%" required min = "0" max = "100" name = "mark5" value = "" ></input></strong></p>
 	</div>
 	
 	<!--MARK 6-->
 	<div class = "box">
 		<h2>Couse 6</h2><p class = "mono">
-		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark6" value = "" ></input></strong></p>
+		MARK: <input type = "number" step = "1" placeholder = "##%" required min = "0" max = "100" name = "mark6" value = "" ></input></strong></p>
 	</div>
 
 <!--CALC SCHOLARSHIP GO BUTTON-->
 <div class = "box" id = "calc">
-<h2>Find A Scholarship For You</h2>
+<h2>Find A Scholarship</h2>
 <input type = "submit" class = "button" name = "go" value = "calculate"></div>
 </form>
 
@@ -143,6 +143,11 @@ if ($_GET['go'] == "calculate") {
 		$getString = "mark" . $r;
 		$mark[$r] = $_GET[$getString];
 	}
+	//LOGIC: CONVERT MARKS TO GPAs
+	
+	//LOGIC: AVERAGE GPA & DETIRMINE SCHOLARSHIP
+	
+	
 	// Print Recipt 
 	echo "<div class = 'receipt'><h3>Marks</h3><BR><BR>";
 	while ($p != count($itemNames, COUNT_RECURSIVE)) {
@@ -162,30 +167,7 @@ if ($_GET['go'] == "calculate") {
 	echo "<span class = 'mono'>Delivery Charge: $" . $deliveryFee . "</span><BR>";
 	echo "<span class = 'mono'>Tax @ 13%: $" . $taxFee . "</span><BR><BR>";
 	echo "<span class = 'total'><strong> Total: " . total($subTotal, $deliveryFee, $taxFee) . "</strong></span";
-	
 	echo "</div>";
-}
-function delivery($subTotal) {
-	if ($subTotal <= 5) {
-		$deliveryCharge = 5.00;
-	}
-	else if ($subTotal > 5 && $subTotal <= 15) {
-		$deliveryCharge = 3.00;
-	}
-	else {
-		$deliveryCharge = 0;
-	}
-	return $deliveryCharge;
-}
-function tax($subTotal){
-	$tax = 0.13 * ((delivery($subTotal)) + $subTotal);
-	$tax = round($tax, 2);
-	return $tax;
-}
-function total($subTotal, $deliveryFee, $taxFee) {
-	$total = $subTotal + delivery($subTotal) + tax($subTotal);
-	$total = round($total, 2);
-	return $total;
 }
 ?>
 </body>
