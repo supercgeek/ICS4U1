1aa754d4bc3d95a0a75403c339eb604e3912003b
supercgeek <cameronbb3@gmail.com>
2015-06-15 16:09:29 -0400

    golo

--- 1_Projects/27_Activity_3/perf-task-1-scholarships.php
+++ 1_Projects/27_Activity_3/perf-task-1-scholarships.php
@@ -10,93 +10,67 @@
 	</head>
 <body>
 <h1>Scholarship Calculator</h1><BR><BR>
-
-<!--
-<div class = "box">
-<img src="http://pixel.brit.co/wp-content/uploads/2013/05/8-BlackBurger.jpg">
-<h2>Cheese Burger</h2>
-<p class = "price"><strong>$8.99</strong></p>
-ORDER: <input type = "number" min = "0" name = "orderVal1" value = "" ></input>
+<div class = "hello">
+<p>
+	This page will calculate the entrance scholarship you will recieve based upon your top six marks.<BR><BR> After the marks are recieved, they will be converted to a 4.0 GPA scale based upon on the below table (fig. 1). Following that, they will be averaged into one GPA and your schaolarship will be calculated based on the below (fig. 2).
+</p>
 </div>
--->
-<form action = "perf-task-2-fastfood.php" method = "get">
+<!--MARK INPUT-->
+	<form action = "perf-task-1-scholarships.php" method = "POST">
+	
+	<!--MARK 1-->
+	<div class = "box">
+		<h2>Couse 1</h2><p class = "mono">
+		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark1" value = "" ></input></strong></p>
+	</div>
+	
+	<!--MARK 2-->
+	<div class = "box">
+		<h2>Couse 2</h2><p class = "mono">
+		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark2" value = "" ></input></strong></p>
+	</div>
+	
+	<!--MARK 3-->
+	<div class = "box">
+		<h2>Couse 3</h2><p class = "mono">
+		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark3" value = "" ></input></strong></p>
+	</div>
+	
+	<!--MARK 4-->
+	<div class = "box">
+		<h2>Couse 4</h2><p class = "mono">
+		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark4" value = "" ></input></strong></p>
+	</div>
+	
+	<!--MARK 5-->
+	<div class = "box">
+		<h2>Couse 5</h2><p class = "mono">
+		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark5" value = "" ></input></strong></p>
+	</div>
+	
+	<!--MARK 6-->
+	<div class = "box">
+		<h2>Couse 6</h2><p class = "mono">
+		MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark6" value = "" ></input></strong></p>
+	</div>
 
-<!--MARK 1-->
-<div class = "box">
-	<h2>Couse 1</h2><p class = "mono">
-	MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark1" value = "" ></input></strong></p>
-</div>
-
-<!--MARK 2-->
-<div class = "box">
-	<h2>Couse 2</h2><p class = "mono">
-	MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark2" value = "" ></input></strong></p>
-</div>
-
-<!--MARK 3-->
-<div class = "box">
-	<h2>Couse 3</h2><p class = "mono">
-	MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark3" value = "" ></input></strong></p>
-</div>
-
-<!--MARK 4-->
-<div class = "box">
-	<h2>Couse 4</h2><p class = "mono">
-	MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark4" value = "" ></input></strong></p>
-</div>
-
-<!--MARK 5-->
-<div class = "box">
-	<h2>Couse 5</h2><p class = "mono">
-	MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark5" value = "" ></input></strong></p>
-</div>
-
-<!--MARK 6-->
-<div class = "box">
-	<h2>Couse 6</h2><p class = "mono">
-	MARK: <input type = "number" step = "1" placeholder = "##%" min = "0" max = "100" name = "mark6" value = "" ></input></strong></p>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--TALLY ORDER-->
-<div class = 'box' id = 'order'>
-<h2>TALLY ORDER</h2>
-<sub>&</sub>
-<h2>PRINT RECEIPT</h2><BR>
-<input type = "submit" class = "button" name = "tally" value = "CLICK!"></div>
+<!--CALC SCHOLARSHIP GO BUTTON-->
+<div class = "box" id = "calc">
+<h2>Find A Scholarship For You</h2>
+<input type = "submit" class = "button" name = "go" value = "calculate"></div>
 </form>
 
 <?php
-if ($_GET['tally'] == "CLICK!") {
+if ($_GET['go'] == "calculate") {
 	
-	
-	
-	//Create Array of Item Quantities Ordered
-	while ($r != count($itemNames, COUNT_RECURSIVE)) {
+	//Create Array of Six Marks
+	while ($r != 6) {
 		$r++;
-		$orderString = "OrderID-" . $r;
-		$itemQuants[$r] = $_GET[$orderString];
+		$getString = "mark" . $r;
+		$mark[$r] = $_GET[$getString];
 	}
 	// Print Recipt 
-	echo "<div class = 'receipt'><h3>RECEIPT</h3><BR><BR>";
-		// echo "<div class = 'nested'>";
+	echo "<div class = 'receipt'><h3>Marks</h3><BR><BR>";
 	while ($p != count($itemNames, COUNT_RECURSIVE)) {
 		$p++;
 		if ($itemQuants[$p] < 1){
@@ -106,7 +80,6 @@ if ($_GET['tally'] == "CLICK!") {
 		$subTotal = $subTotal + $currentItemTotal;
 		echo "<strong>" . $itemNames[$p] . "</strong><BR><span class = 'mono'> X"   . $itemQuants[$p] . " @ $" . $itemPrices[$p] . " =  $" . $currentItemTotal . "</span><BR><BR>";
 		}
-		// echo "</div>";
 	}
 	// LOGIC ON COMPUTING DELIVERARY CHARGES
 	$deliveryFee = delivery($subTotal);
