11396673fde3bd7d25ec95dda5ea6c443711e3f5
supercgeek <cameronbb3@gmail.com>
2015-06-22 00:43:50 -0400

    yeet

--- 1_Projects/25_Activity_2-8/Santa_Claus/santas-workshop.php
+++ 1_Projects/25_Activity_2-8/Santa_Claus/santas-workshop.php
@@ -5,9 +5,9 @@
 		<script src="http://use.edgefonts.net/lekton:n7,i4,n4:all.js"></script>
 </head>
 <body>
-	<h3><a href = "playing-santa.php">Return to Playing Santa</a></h3>
 	<h1 id = "big">Santa's Workshop</h1>
 	<div class = "all">
+			<h3><a href = "playing-santa.php">Return to Playing Santa</a></h3><BR><BR>
 	<h3><a href = "kid_list.php">Edit The Kid List</a></h3>
 	<h3><a href = "toy_list.php">Edit The Toy List</a></h3>	
 	<div>
